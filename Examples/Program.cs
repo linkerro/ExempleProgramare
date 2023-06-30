8eb0cbbb5c85cef7fa6035b9f3e0a423ff4a9419
Radu Iscu <linkerro@gmail.com>
2023-06-30 22:19:34 +0300

    Codul cu exemplele pentru if si while

---
+++ Examples/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Examples
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string raspuns;
+            int raspuns_numar_intreg;
+
+            Console.WriteLine("Gandeste-te la un numar mai mic decat 50 dar mai mare decat 10.");
+            Console.Write("Care este acel numar: ");
+
+            raspuns = Console.ReadLine();
+            bool verificare = int.TryParse(raspuns, out raspuns_numar_intreg);
+            if (verificare)
+            {
+                if (raspuns_numar_intreg < 10)
+                {
+                    Console.WriteLine("Numarul este prea mic.");
+                }
+                else if (raspuns_numar_intreg > 50)
+                {
+                    Console.WriteLine("Numarul este prea mare.");
+                }
+                else
+                {
+                    Console.WriteLine("Raspuns valid.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("fuck of coaie");
+            }
+
+        }
+    }
+}
---
+++ WhileExample/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace WhileExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            Console.WriteLine("Ghici la ce numar m-am gandit");
+            int numarGandit = 10;
+            while (x != numarGandit) 
+            {
+                string inputUtilizator = Console.ReadLine();
+                bool parsareaAFostCorecta = int.TryParse(inputUtilizator, out x);
+                if (parsareaAFostCorecta)
+                {
+                    if (x > numarGandit)
+                    {
+                        Console.WriteLine("Numarul tau este prea mare! Mai incearca!");
+                    }
+                    else if (x < numarGandit)
+                    {
+                        Console.WriteLine("Numarul tau este prea mic! Mai incearca!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("NAILED IT BRAH");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Numar, cwae!");
+                }
+            }
+        }
+    }
+}
