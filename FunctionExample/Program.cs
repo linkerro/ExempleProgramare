7fcc819fdebe12f5601b08b15a7ac31b8472df89
Radu Iscu <linkerro@gmail.com>
2023-07-08 10:33:33 +0300

    Added function example

---
+++ FunctionExample/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FunctionExample
+{
+    internal class Program
+    {
+        /*
+         * tema: modificati functia Factorial
+         * in asa fel incat sa nu crape cand este apelata
+         * cu numere mai mici decat 1 (e.g. 0, -1, -42, etc.)
+         */
+
+        static void Main(string[] args)
+        {
+            int fact = Factorial(-100);
+            Console.WriteLine(fact);
+
+        }
+
+        static int Factorial(int i)
+        {
+            if (i == 1)
+            {
+                return 1;
+            }
+
+            int result = i * Factorial(i - 1);
+            return result;
+        }
+    }
+}
